8937f89177a97adaef757a9e2f75c8daa016f180
HimeshDua <himeshcancode@gmail.com>
2025-06-05 13:43:40 +0500

    some deploymet issues fixed! try no 5 hydrations errors of next-themes

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
-import {ComponentProps, useEffect, useState} from 'react';
+import {ComponentProps, useEffect, useLayoutEffect, useState} from 'react';
 
 export function ThemeProvider({
   children,
   ...props
 }: ComponentProps<typeof NextThemesProvider>) {
-  // const [mounted, setMounted] = useState(false);
-  // useEffect(() => {
-  //   if (!mounted) {
-  //     setMounted(true);
-  //   }
-  // }, []);
+  const [mounted, setMounted] = useState(false);
+  useLayoutEffect(() => {
+    if (!mounted) {
+      setMounted(true);
+    }
+  }, []);
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
