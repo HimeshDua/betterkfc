a7c6825734b71e9b2b0546d271f17e21756ecfcc
HimeshDua <himeshcancode@gmail.com>
2025-06-07 21:52:27 +0500

    dsd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import ProductCardHome from '@/components/ProductCardHome';
+// import ProductCardHome from '@/components/ProductCardHome';
 import ProductCardCircle from '@/components/ProductCardCircle';
 
 export default function Home() {
